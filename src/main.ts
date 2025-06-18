531dd198f225acabb835fac78a92156fd5045373
Ayesha Rafiq <ayesharafiq151342@gmail.com>
2025-06-18 19:56:26 +0500

    fix: use process.env.PORT fallback for Railway

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
+import './polyfill'
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from "./app.module";
 import { ConfigService } from '@nestjs/config';
 import * as express from 'express';
-import './polyfill'
+
 // console.log("✅ App starting...");
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Get config service from NestJS app context
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+ const port = process.env.PORT || configService.get<number>('PORT') || 3000;
+
 
   // Enable CORS for frontend (e.g., Vue on localhost:5173)
   app.enableCors({
